876a21338ebb17bf8d5c40e533a32b3c6950a9db
Zerok00 <lucarellidavide5@gmail.com>
2024-02-20 18:06:46 +0100

    Prove sul csv

--- webapp/test.py
+++ webapp/test.py
@@ -3,16 +3,16 @@ from flask import Flask, jsonify, render_template, request, redirect, url_for
 import folium
 
 app = Flask(__name__)
-
 @app.route("/")
 def homepage():
-    mappa = folium.Map([45.465, 9.185], zoom_start=7)
+    mappa = folium.Map([45.51, 9.75], zoom_start=8)
     mappa.get_root().width = "800px"
     set_stazioni = set()
     with open("../database/dati/lombardia_qa/Stazioni_qualit__dell_aria_20240220.csv", "r") as file:
         lettore = csv.reader(file)
         next(lettore)
         for elem in lettore:
+            print(elem)
             if elem[3] not in set_stazioni:
                 folium.Marker(location=[elem[13], elem[14]], icon=folium.Icon(color="green", icon="square-check", prefix="fa")).add_to(mappa)
                 set_stazioni.add(elem[3])
