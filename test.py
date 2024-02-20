5ca4314155b07687ca44869e5f7d71e634212ed9
Zerok00 <lucarellidavide5@gmail.com>
2024-02-20 12:09:21 +0100

    Modificate dimensioni mappa

--- test.py
+++ test.py
@@ -2,14 +2,13 @@ from flask import Flask, jsonify, render_template, request, redirect, url_for
 import folium
 
 app = Flask(__name__)
-@app.route("/ciao")
+@app.route("/")
 def homepage():
-    mappa = folium.Map([42, 12.5], zoom_start=6)
-    mappa.get_root().width = "800px"
-    mappa.get_root().height = "600px"
-    folium.Marker(location=[41.89,12.492]).add_to(mappa)
+    mappa = folium.Map([45.465, 9.185], zoom_start=8) #mappa centrata su Milano che inquadra la lombardia
+    mappa.get_root().width = "40%"
+    folium.Marker(location=[45.464,9.191]).add_to(mappa)    #segnalino sul duomo di milano
     iframe = mappa.get_root()._repr_html_()
-    #return render_template("homepage.html", iframe=iframe)
+    return render_template("homepage.html", iframe=iframe)
 
 if __name__ == '__main__':
     app.run(debug=True)
