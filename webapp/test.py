148371e2cbcb4c9b52b8d4886cc1803aa1f0da20
Zerok00 <lucarellidavide5@gmail.com>
2024-02-20 16:36:48 +0100

    Puntatori corretti

--- webapp/test.py
+++ webapp/test.py
@@ -1,15 +1,23 @@
+import csv
 from flask import Flask, jsonify, render_template, request, redirect, url_for
 import folium
 
 app = Flask(__name__)
-#ciao ciao
+
 @app.route("/")
 def homepage():
-    mappa = folium.Map([42, 12.5], zoom_start=6)
+    mappa = folium.Map([45.465, 9.185], zoom_start=7)
     mappa.get_root().width = "800px"
-    print("ciao")
-    folium.Marker(location=[41.89,12.492]).add_to(mappa)
-    print("hola")
+    set_stazioni = set()
+    with open("../database/dati/lombardia_qa/Stazioni_qualit__dell_aria_20240220.csv", "r") as file:
+        lettore = csv.reader(file)
+        next(lettore)
+        for elem in lettore:
+            if elem[3] not in set_stazioni:
+                folium.Marker(location=[elem[13], elem[14]], icon=folium.Icon(color="green", icon="square-check", prefix="fa")).add_to(mappa)
+                set_stazioni.add(elem[3])
+    folium.GeoJson("../database/dati/lombardy.geojson").add_to(mappa)
+    iframe = mappa.get_root()._repr_html_()
     return render_template("homepage.html", iframe=iframe)
 
 if __name__ == '__main__':
