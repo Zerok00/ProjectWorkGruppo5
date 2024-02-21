6fdeeb1d061b1625c7134125b2e1d4b719b2850e
Zerok00 <lucarellidavide5@gmail.com>
2024-02-21 12:22:10 +0100

    correzione test

--- webapp/test.py
+++ webapp/test.py
@@ -4,19 +4,6 @@ import folium
 
 app = Flask(__name__)
 
-# with open("../database/dati/lombardia_qa/accentra.CSV", "r") as file:
-#     lettore = csv.reader(file, delimiter=";")
-#     next(lettore)
-#     diz_stazioni = {}
-#     for elem in lettore:
-#         diz_stazioni[elem[3]]=[]
-# with open("../database/dati/lombardia_qa/accentra.CSV", "r") as file:
-#     lettore = csv.reader(file, delimiter=";")
-#     next(lettore)
-#     for elem in lettore:
-#         diz_stazioni[elem[3]].append(f"{elem[1]}:{elem[5]}")
-#
-# print(diz_stazioni)               risulta che la quota di ogni sensore è uguale nella stessa stazione, quindi l'AQI è già calcolato
 @app.route("/")
 def homepage():             #calcolare i valori AQI
     mappa = folium.Map([45.51, 9.75], zoom_start=8)
@@ -65,13 +52,5 @@ def homepage():             #calcolare i valori AQI
                                       popup="Hazardous").add_to(mappa)
                         set_stazioni.add(elem[3])
     folium.GeoJson("../database/dati/lombardy.geojson").add_to(mappa)
-@app.route("/")
-def homepage():
-    mappa = folium.Map([45.465, 9.185], zoom_start=8) #mappa centrata su Milano che inquadra la lombardia
-    mappa.get_root().width = "40%"
-    folium.Marker(location=[45.464,9.191]).add_to(mappa)    #segnalino sul duomo di milano
-    iframe = mappa.get_root()._repr_html_()
-    return render_template("homepage.html", iframe=iframe)
-
 if __name__ == '__main__':
     app.run(debug=True)
