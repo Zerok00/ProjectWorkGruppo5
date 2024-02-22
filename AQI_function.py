c596fd0a7dc6a9baca0400d4466e82c21d3746cc
giovannisucameli97 <48803291+giovannisucameli97@users.noreply.github.com>
2024-02-22 11:56:00 +0100

    ciao

--- AQI_function.py
+++ AQI_function.py
@@ -1,5 +1,5 @@
 def aqi_function():
-    aqi_levels = [
+    epa_tables = [
 
         {"AQI Range": [0.0, 50.0], "PM10": [0.0, 54.], "PM2.5": [0.0, 9.0], "O3 1-hr": "-", "CO": [0.0, 4.4],
          "SO2": [0.0, 35.0], "NO2": [0.0, 53.0], "Description": "Good"},
@@ -16,8 +16,18 @@ def aqi_function():
         {"AQI Range": [201., 300.0], "PM10": [355.0, 424.0], "PM2.5": [125.5, 225.4], "O3 1-hr": [205.0, 404.0],
          "CO": [15.5, 30.4], "SO2": [305.0, 604.0], "NO2": [650.0, 1249.0], "Description": "Very Unhealthy"},
 
-
         {"AQI Range": [301., 500.], "PM10": [425., 604.0], "PM2.5": [225.5, 325.4], "O3 1-hr": [405., 604.0], "CO": [30.5, 50.4],
          "SO2": [605., 1004.0], "NO2": [1250.0, 2049.0], "Description": "Hazardous"}
 
     ]
+
+
+    #conversione O3 da ppb a mug/m3
+    for i in range(len(epa_tables)):
+        if epa_tables[i]["O3 1-hr"] != "-":
+            epa_tables[i]["O3 1-hr"][0] = epa_tables[i]["O3 1-hr"][0]*20.30
+            epa_tables[i]["O3 1-hr"][1] = epa_tables[i]["O3 1-hr"][1]*20.30
+
+    print(epa_tables)
+
+aqi_function()
