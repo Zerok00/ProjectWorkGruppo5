e2d8fcd5a18b3160154ee850d4a91168a564d44c
ale-comi <alecomi95@gmail.com>
2024-02-25 18:26:01 +0100

    update inserimento, analisi frequenza da completare (vedi pdf)

--- migration/database_sql/db_controller.py
+++ migration/database_sql/db_controller.py
@@ -13,11 +13,11 @@ except Error as e:
 
 
 path = 'data\Stazioni_qualit__dell_aria_20240220.csv'
-operations.clean_csv_stazioni(path, execute=True)
+operations.clean_csv_stazioni(path, execute=False)
 
 path_stazioni = 'data\data_clean\dataset_pulito_stazioni.csv'
 path = 'data\Dati_sensori_aria_20240219.csv'
-operations.clean_csv_rilevazioni(path, path_stazioni, execute=True)
+operations.clean_csv_rilevazioni(path, path_stazioni, execute=False)
 
 path_rilevazioni = 'data\data_clean\dataset_pulito_rilevazioni.csv'
 
