a76ead4fc3ef55498e31ae8a33068cd36659ba6c
giovannisucameli97 <48803291+giovannisucameli97@users.noreply.github.com>
2024-02-27 18:10:56 +0100

    jabckjahb

--- webapp/grafico_media_mobile_tasti.py
+++ webapp/grafico_media_mobile_tasti.py
@@ -7,54 +7,55 @@ import pandas as pd
 #QUERY PER SELZIONARE DATI DA PRENDERE (STAZIONE E POLLUTIONS)
 #query =
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
-df.columns = [col.replace('AAPL.', '') for col in df.columns]
+df = pd.read_csv('rilevazione1.csv')
+print(df)
+#df.columns = [col.replace('AAPL.', '') for col in df.columns]
 
-trace_high = go.Scatter(x=list(df.index),
-                        y=list(df.High),
-                        name='High',
-                        line=dict(color='#33CFA5'))
+trace = go.Scatter(x=list(df.data),
+                        y=list(df.valore),
+                        name='Valore',
+                        marker=dict(color='#33CFA5'))
 
-trace_high_avg = go.Scatter(x=list(df.index),
-                            y=[df.High.mean()]*len(df.index),
-                            name='High Average',
-                            visible=False,
-                            line=dict(color='#33CFA5', dash='dash'))
+# trace_high_avg = go.Scatter(x=list(df.index),
+#                             y=[df.High.mean()]*len(df.index),
+#                             name='High Average',
+#                             visible=False,
+#                             line=dict(color='#33CFA5', dash='dash'))
+#
+# trace_low = go.Scatter(x=list(df.index),
+#                        y=list(df.Low),
+#                        name='Low',
+#                        line=dict(color='#F06A6A'))
+#
+# trace_low_avg = go.Scatter(x=list(df.index),
+#                            y=[df.Low.mean()]*len(df.index),
+#                            name='Low Average',
+#                            visible=False,
+#                            line=dict(color='#F06A6A', dash='dash'))
 
-trace_low = go.Scatter(x=list(df.index),
-                       y=list(df.Low),
-                       name='Low',
-                       line=dict(color='#F06A6A'))
-
-trace_low_avg = go.Scatter(x=list(df.index),
-                           y=[df.Low.mean()]*len(df.index),
-                           name='Low Average',
-                           visible=False,
-                           line=dict(color='#F06A6A', dash='dash'))
-
-data = [trace_high, trace_high_avg, trace_low, trace_low_avg]
-
-high_annotations=[dict(x='2016-03-01',
-                       y=df.High.mean(),
-                       xref='x', yref='y',
-                       text='High Average:<br>'+str(df.High.mean()),
-                       ax=0, ay=-40),
-                  dict(x=df.High.idxmax(),
-                       y=df.High.max(),
-                       xref='x', yref='y',
-                       text='High Max:<br>'+str(df.High.max()),
-                       ax=0, ay=-40)]
-low_annotations=[dict(x='2015-05-01',
-                      y=df.Low.mean(),
-                      xref='x', yref='y',
-                      text='Low Average:<br>'+str(df.Low.mean()),
-                      ax=0, ay=40),
-                 dict(x=df.High.idxmin(),
-                      y=df.Low.min(),
-                      xref='x', yref='y',
-                      text='Low Min:<br>'+str(df.Low.min()),
-                      ax=0, ay=40)]
-
+data = [ trace, [], [], [] ]
+#
+# high_annotations=[dict(x='2016-03-01',
+#                        y=df.High.mean(),
+#                        xref='x', yref='y',
+#                        text='High Average:<br>'+str(df.High.mean()),
+#                        ax=0, ay=-40),
+#                   dict(x=df.High.idxmax(),
+#                        y=df.High.max(),
+#                        xref='x', yref='y',
+#                        text='High Max:<br>'+str(df.High.max()),
+#                        ax=0, ay=-40)]
+# low_annotations=[dict(x='2015-05-01',
+#                       y=df.Low.mean(),
+#                       xref='x', yref='y',
+#                       text='Low Average:<br>'+str(df.Low.mean()),
+#                       ax=0, ay=40),
+#                  dict(x=df.High.idxmin(),
+#                       y=df.Low.min(),
+#                       xref='x', yref='y',
+#                       text='Low Min:<br>'+str(df.Low.min()),
+#                       ax=0, ay=40)]
+#
 updatemenus = list([
     dict(type="buttons",
          active=-1,
@@ -63,29 +64,29 @@ updatemenus = list([
                  method = 'update',
                  args = [{'visible': [True, True, False, False]},
                          {'title': 'Yahoo High',
-                          'annotations': high_annotations}]),
+                          'annotations': ""}]),
             dict(label = 'Low',
                  method = 'update',
                  args = [{'visible': [False, False, True, True]},
                          {'title': 'Yahoo Low',
-                          'annotations': low_annotations}]),
+                          'annotations': ""}]),
             dict(label = 'Both',
                  method = 'update',
                  args = [{'visible': [True, True, True, True]},
                          {'title': 'Yahoo',
-                          'annotations': high_annotations+low_annotations}]),
+                          'annotations': ""}]),
             dict(label = 'Reset',
                  method = 'update',
                  args = [{'visible': [True, False, True, False]},
                          {'title': 'Yahoo',
-                          'annotations': []}])
+                          'annotations': ""}])
         ]),
     )
 ])
 
-layout = dict(title='Yahoo', showlegend=False,
+layout = dict(title='Provaaaaaaaaaaaaaaaaaaaaaaaaaaa', showlegend=False,
               updatemenus=updatemenus)
 
 fig = dict(data=data, layout=layout)
-plot(fig, filename='update_button')
+plot(fig, filename='rilevazione.html')
 # plot(fig)
