35b359e2cc40880bc1e2081253c750884c8cd2cd
giovannisucameli97 <48803291+giovannisucameli97@users.noreply.github.com>
2024-02-27 10:31:03 +0100

    adjfna

---
+++ webapp/lista_comuni_accettabili.py
@@ -0,0 +1,31 @@
+#Funzione per la creazione della lista dei comuni accettabili
+import csv
+# importing geopy library
+from geopy.geocoders import Nominatim
+# Importing the geodesic module from the library
+from geopy.distance import geodesic
+
+with open('comuni_accettabili.txt', mode='r') as tsv_file:
+    tsv_reader = csv.reader(tsv_file, delimiter='\t')
+    next(tsv_reader)
+    lista_comuni = list(tsv_reader)
+
+print(lista_comuni)
+print(len(lista_comuni))
+
+
+# calling the Nominatim tool
+loc = Nominatim(user_agent="GetLoc")
+
+lista_problemi = []
+for elem in lista_comuni:
+    getLoc = loc.geocode(elem[0], timeout=10)
+    indirizzo = getLoc.address
+    if "Lombardia" not in indirizzo or indirizzo == "":
+        lista_problemi.append((indirizzo, elem))
+    print(indirizzo)
+    print(lista_problemi)
+
+print(lista_problemi)
+
+
